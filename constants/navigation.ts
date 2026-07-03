b05e5715f1c91315025efb6dced606c75fee4e58
Aqsa Raisnaldi <yazidar27@gmail.com>
2026-07-03 23:47:25 +0700

    refactor: organize navbar structure

---
+++ constants/navigation.ts
@@ -0,0 +1,26 @@
+export const NAV_LINKS = [
+  {
+    label: "Home",
+    href: "#home",
+  },
+  {
+    label: "Programs",
+    href: "#programs",
+  },
+  {
+    label: "Certification",
+    href: "#certification",
+  },
+  {
+    label: "Achievement",
+    href: "#achievement",
+  },
+  {
+    label: "About",
+    href: "#about",
+  },
+  {
+    label: "Contact",
+    href: "#contact",
+  },
+];
